0e854749f570c401aaeb0a2a7b6b5487733b408d
lu06528 <luca.schaepers@studmail.w-hs.de>
2024-04-12 16:45:20 +0200

    WebApp is in an usable state

--- database.py
+++ database.py
@@ -1,38 +1,79 @@
 import sqlite3
+import datetime
 
-db = sqlite3.connect("recipes.db")
+myDB = sqlite3.connect("recipes.db")
 
-mycursor = db.cursor()
+mycursor = myDB.cursor()
 
+# TODO: Database needs a 'Last time cooked' column
 def initializeDB():
-    mycursor.execute("CREATE TABLE IF NOT EXISTS recipes(Name TINYTEXT, Vegetarisch BIT, Papa BIT, PRIMARY KEY (Name))")
-    db.commit()
+    mycursor.execute("CREATE TABLE IF NOT EXISTS recipes(Name TINYTEXT, Vegetarisch BIT, Papa BIT, Zuletzt DATETIME, PRIMARY KEY (Name));")
+    myDB.commit()
 
 def clearDB():
-    mycursor.execute("DROP TABLE recipes")
-    db.commit()
+    mycursor.execute("DROP TABLE recipes;")
+    myDB.commit()
     initializeDB()
 
-def addRecipe(name: str, veggie: int, dad: int) -> None:
-    mycursor.execute(f"INSERT OR IGNORE INTO recipes(Name, Vegetarisch, Papa) VALUES('{name}', {veggie}, {dad})")
+def addRecipe(db: sqlite3.Connection, cursor: sqlite3.Cursor, name: str, veggie: int, dad: int) -> None:
+    today = datetime.date.today()
+    cursor.execute(f"INSERT OR IGNORE INTO recipes(Name, Vegetarisch, Papa, Zuletzt) VALUES('{name}', {veggie}, {dad}, '{today.strftime("%d.%m.%Y")}');")
 
     db.commit()
 
-def printRecipes(filter):
-    query = "SELECT Name FROM recipes"
+def updateRecipeDate(cursor: sqlite3.Cursor, name: str, date: datetime.date):
+    cursor.execute(f"UPDATE recipes SET Zuletzt = '{date}' WHERE Name = '{name}';")
+
+    myDB.commit()
+
+def updateRecipeToday(db: sqlite3.Connection, cursor: sqlite3.Cursor, name: str):
+    today = datetime.date.today()
+    cursor.execute(f"UPDATE recipes SET Zuletzt = '{today.strftime("%d.%m.%Y")}' WHERE Name = '{name}';")
+
+    db.commit()
+
+def fetchRecipes(cursor: sqlite3.Cursor, filter):
+    query = "SELECT Name, Zuletzt FROM recipes"
     if filter != 0:
         type = (
             "Vegetarisch" if filter == 1 else
             "Papa" if filter == 2 else
             0
         )
-        query += f" WHERE {type} = 1 ORDER BY Name"
-        
-    printQueryResults(query)
+        query += f" WHERE {type} = 1"
+    query += " ORDER BY Zuletzt ASC, Name;"
 
-def printQueryResults(query):
+    cursor.execute(query)
+    result = []
+    for(Rezept) in cursor:
+        rezeptTupel = (Rezept[0], Rezept[1])
+        result.append(rezeptTupel)
+    return result
+   
+#    printQueryResults(query)
+#
+#def printQueryResults(query):
+#    mycursor.execute(query)
+#    for result in mycursor:
+#        print(result[0])
+
+# def fetchQueryResults(query):
+
+def copyRecipesToDB():
+    # Lese Zeilen aus Text-Datei für Rezepte
+    file = open("./recipes.txt", "r")
+    Lines = file.readlines()
+    for(line) in Lines:
+        lineValues = line.split('|')
+        addRecipe(mycursor, lineValues[0], int(lineValues[1]), int(lineValues[2]))
+
+def printAll():
+    print("Inhalt der Datenbank:")
+    query = "SELECT Name, Zuletzt FROM recipes ORDER BY Zuletzt ASC, Name;"
     mycursor.execute(query)
     for result in mycursor:
-        print(result[0])
+        print(result[0] + " | " + result[1])
 
-initializeDB()
+#clearDB()
+#copyRecipesToDB()
+#printAll()
--- findRecipes.py
+++ findRecipes.py
@@ -1,17 +1,90 @@
-
 from database import *
 from recipe import *
+from flask import Flask, render_template, request
+from werkzeug.local import Local
+#from jinja2 import Environment, PackageLoader, select_autoescape
+#env = Environment(
+#    loader=PackageLoader("findRecipes"),
+#    autoescape=select_autoescape()
+#)
+#template = env.get_template("template.html")
 
-def copyRecipesToDB():
-    file = open("./recipes.txt", "r")
-    Lines = file.readlines()
-    for(line) in Lines:
-        lineValues = line.split('|')
-        addRecipe(lineValues[0], int(lineValues[1]), int(lineValues[2]))
+app = Flask(__name__)
+local = Local()
 
-#clearDB()
+def get_db():
+    if not hasattr(local, 'recipes_db'):
+        local.recipes_db = sqlite3.connect('recipes.db')
+    return local.recipes_db
 
-copyRecipesToDB()
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(local, 'recipes_db'):
+        local.recipes_db.close()
 
-#Parameters: All 0 | Vegetarian 1 | Dad would eat it 2
-printRecipes(1)
+@app.route('/')
+def index():
+    '''Zeigt die Startseite an (Bisher leer).'''
+    return render_template('/index.html')
+
+@app.route('/filter')
+def unfiltered_recipes():
+    '''Zeigt die Page mit allen Rezepten an.'''
+    db = get_db()
+    mycursor = db.cursor()
+    rows = fetchRecipes(mycursor, 0)
+    return render_template('/template.html', rows=rows)
+
+@app.route('/filter', methods=['POST'])
+def filtered_recipes():
+    '''Zeigt die Page mit den gefilterten Rezepten an.'''
+    # Verbindung zur Datenbank herstellen
+    db = get_db()
+    mycursor = db.cursor()
+    # db_filter sollte die Werte 0, 1 oder 2 haben
+    try:
+        db_filter = request.form['filter']
+    except:
+        db_filter = 0
+    rows = fetchRecipes(mycursor, int(db_filter))
+    # HTML-Vorlage mit Jinja2 rendern  und Daten einfügen
+    return render_template('/template.html', rows=rows)
+
+@app.route('/updateRecipe', methods=['POST'])
+def update_recipe():
+    '''Für das übergebene Rezept wird das 'Zuletzt' Datum in der
+    Datenbank aktualisiert.'''
+    # Verbindung zur Datenbank herstellen
+    db = get_db()
+    mycursor = db.cursor()
+    # Speichere Name des zu aktualisierenden Rezeptes
+    recipeName = request.form['updateButton']
+    # Aktualisiere Datum
+    updateRecipeToday(db, mycursor, recipeName)
+    # Lade Rezepte Page neu
+    rows = fetchRecipes(mycursor, 0)
+    return render_template('/template.html', rows=rows)
+
+@app.route('/addRecipe')
+def add_recipe_page():
+    '''Zeige die Page 'addRecipe' an.'''
+    return render_template('/addRecipe.html')
+
+@app.route('/recipeAdded', methods=['POST'])
+def add_recipe():
+    '''Die Daten, die durch das Form übertragen werden, werden zur Datenbank hinzugefügt.'''
+    # Verbindung zur Datenbank herstellen
+    db = get_db()
+    mycursor = db.cursor()
+    # Speichere Name des zu aktualisierenden Rezeptes
+    recipeName = request.form['recipeName']
+    veggie = int(request.form['veggie'])
+    dadApproved = int(request.form['dadApproved'])
+    # Füge Rezept zur Datenbank hinzu
+    addRecipe(db, mycursor, recipeName, veggie, dadApproved)
+    # Lade Rezepte Page neu
+    rows = fetchRecipes(mycursor, 0)
+    return render_template('/template.html', rows=rows)
+
+if __name__ == '__main__':
+    app.run(debug=True)
